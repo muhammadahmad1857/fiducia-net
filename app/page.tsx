7a971566c4d1622700e359d2b20dd0e529fbd499
Ahmad Jawad <ahmedjawad1857@gmail.com>
2024-10-02 00:58:31 +0500

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { client } from "@/sanity/lib/client";
 import React from "react";
 
-const fetchData = async () => {
-  const data = await client.fetch('*[_type == "reviews"]');
-  console.log(data);
-  return data;
-};
+// const fetchData = async () => {
+//   const data = await client.fetch('*[_type == "reviews"]');
+//   console.log(data);
+//   return data;
+// };
 
 const Home = async () => {
-  const data = await fetchData();
+  // const data = await fetchData();
   return <div></div>;
 };
 
