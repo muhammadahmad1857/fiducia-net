32913a1bcb9596a2c1d2a6c88c48569028e7e1b9
Ahmad Jawad <ahmedjawad1857@gmail.com>
2024-10-06 16:48:03 +0500

    fixed build err

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,4 @@
 import { type SchemaTypeDefinition } from "sanity";
-import pet from "./pet";
 import reviews from "./reviews";
 import services from "./services";
 import projects from "./projects";
