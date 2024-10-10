bdf9358d949fc97213cc96d5229b891b8c48c4b4
Ahmad Jawad <ahmedjawad1857@gmail.com>
2024-10-10 17:35:07 +0500

    make route.ts a module

--- app/api/addSubscription/route.ts
+++ app/api/addSubscription/route.ts
@@ -1,26 +1,25 @@
-// import mailchimp from "@mailchimp/mailchimp_marketing";
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
-// mailchimp.setConfig({
-//   apiKey: process.env.MAILCHIMP_API_KEY,
-//   server: process.env.MAILCHIMP_API_SERVER, // e.g. us1
-// });
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: process.env.MAILCHIMP_API_SERVER, // e.g. us1
+});
 
-// export async function POST(request: Request) {
-//   const { email } = await request.json();
+export async function POST(request: Request) {
+  const { email } = await request.json();
 
-//   if (!email) new Response(JSON.stringify({ error: "Email is required" }));
+  if (!email) new Response(JSON.stringify({ error: "Email is required" }));
 
-//   try {
-//     const res = await mailchimp.lists.addListMember(
-//       process.env.MAILCHIMP_AUDIENCE_ID!,
-//       { email_address: email, status: "subscribed" }
-//     );
+  try {
+    const res = await mailchimp.lists.addListMember(
+      process.env.MAILCHIMP_AUDIENCE_ID!,
+      { email_address: email, status: "subscribed" }
+    );
 
-//     return new Response(JSON.stringify({ res }));
-//   } catch (error: any) {
-//     return new Response(
-//       JSON.stringify({ error: JSON.parse(error.response.text) })
-//     );
-//   }
-// }
-
+    return new Response(JSON.stringify({ res }));
+  } catch (error: any) {
+    return new Response(
+      JSON.stringify({ error: JSON.parse(error.response.text) })
+    );
+  }
+}
