b62a279e26a0fd5a7aea08b260d837ac4e07835e
= <ahmedjawad1857@gmail.com>
2024-10-12 15:47:17 +0500

    changed heading main

--- components/header.tsx
+++ components/header.tsx
@@ -5,9 +5,7 @@ const Header = ({ title }: { title: string }) => {
     <div className="pb-20 img-container w-screen ">
       <header className="flex   justify-center text-center pt-40   items-center">
         <div className="flex flex-col gap-5">
-          <h1 className="sm:text-5xl text-3xl font-bold  text-orange-500">
-            {title}
-          </h1>
+          <h1 className="sm:text-5xl text-3xl font-bold">{title}</h1>
           {/* <Socials /> */}
         </div>
       </header>
