05aa63ef459489e4388b919af6a7421da10fde8d
= <ahmedjawad1857@gmail.com>
2024-10-16 20:42:49 +0500

    fixed build err

--- components/animations/revealOnScroll.tsx
+++ components/animations/revealOnScroll.tsx
@@ -1,7 +1,7 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import { gsap } from "gsap";
-import {useGSAP} from "@gsap/react"
+import { useGSAP } from "@gsap/react";
 interface RevealOnScrollProps {
   children: React.ReactNode;
 }
@@ -12,25 +12,29 @@ const RevealOnScroll: React.FC<RevealOnScrollProps> = ({ children }) => {
   useGSAP(() => {
     if (containerRef.current) {
       const element = containerRef.current;
-        gsap.from(element, {
-          scrollTrigger: {
-            trigger: element,
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true,
-            pin: true,
-            snap: 1 / 2, // 50% of the viewport
-            // markers: true,
-          },
-          opacity: 0,
-          scale: 0.5,
-          duration: 1.5,
-          ease: "power2.out",
-        });
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+          pin: true,
+          snap: 1 / 2, // 50% of the viewport
+          // markers: true,
+        },
+        opacity: 0,
+        scale: 0.5,
+        duration: 1.5,
+        ease: "power2.out",
+      });
     }
   }, []);
 
-  return <div ref={containerRef} className="revealOnScroll">{children}</div>;
+  return (
+    <div ref={containerRef} className="revealOnScroll">
+      {children}
+    </div>
+  );
 };
 
 export default RevealOnScroll;
